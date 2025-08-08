c6d92a19a9f984884c9067e5b35024b66f939873
Ali <asafari72@gmail.com>
2025-08-09 01:09:43 +0330

    Add motion library and update PodcastItem component with animations

--- apps/radioscript.live/src/app/(root)/podcasts/page.tsx
+++ apps/radioscript.live/src/app/(root)/podcasts/page.tsx
@@ -6,15 +6,11 @@ export default function Podcasts() {
     <div className="app-container py-4">
       <h1 className="text-3xl font-black mb-8 mt-4">پادکست ها</h1>
 
-      {items.map((i) => (
-        <PodcastItem
-          key={i}
-          href="/podcasts/اپیزود-صفر"
-          caption="اپیزود صفر، باید شروع کرد"
-          description="اثین ثسمت از رادیو اسکریپت درباره شروع برنامه نویسی و چالش هاش هست"
-          feature_image="/images/cover.webp"
-        />
-      ))}
+      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-6 ">
+        {items.map((i) => (
+          <PodcastItem key={i} href="/podcasts/اپیزود-صفر" caption="اپیزود صفر، باید شروع کرد" feature_image="/images/cover.webp" />
+        ))}
+      </div>
     </div>
   );
 }
--- libs/components/src/lib/podcast-item/PodcastItem.tsx
+++ libs/components/src/lib/podcast-item/PodcastItem.tsx
@@ -1,20 +1,30 @@
+'use client';
+import { motion } from 'framer-motion';
+import { PlayCircle } from 'iconsax-react';
 import Image from 'next/image';
 import Link from 'next/link';
 export interface Podcast {
   href: string;
   caption: string;
   feature_image: string;
-  description: string;
 }
-export function PodcastItem({ href, caption, feature_image, description }: Podcast) {
+export function PodcastItem({ href, caption, feature_image }: Podcast) {
   return (
-    <Link href={href} className="flex gap-4 mb-4">
-      <Image src={feature_image} width={120} height={120} className="rounded-2xl cursor-pointer" alt="Radio Script logo" />
-      <div>
-        <div className="mt-2 opacity-30">مدت: 30 دقیقه ، 2 روز پیش</div>
-        <h3 className="text-2xl my-2 font-bold">{caption}</h3>
-        <p className="opacity-50">{description}</p>
-      </div>
-    </Link>
+    <motion.button whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.95 }}>
+      <Link href={href} className="flex flex-col gap-4 m-6 overflow-hidden duration-300 group max-h-72 group">
+        <div className="relative w-full h-52 overflow-hidden flex items-end justify-center rounded-md">
+          <Image src={feature_image} objectFit="cover" layout="fill" alt="Radio Script logo" />
+          <div className="absolute inset-0 bg-gradient-to-t from-black/70 to-transparent"></div>
+          <div className="absolute bottom-4 start-2 flex items-center gap-2 invisible group-hover:visible transition-all duration-300 text-white">
+            <PlayCircle size="32" color="#fff" variant="Bold" />
+            گوش کنید
+          </div>
+        </div>
+        <div className="flex flex-col gap-2 items-start">
+          <h2 className="text-base font-bold">{caption}</h2>
+          <div className="text-sm transition-all duration-300 opacity-50">مدت: 30 دقیقه ، 2 روز پیش</div>
+        </div>
+      </Link>
+    </motion.button>
   );
 }
